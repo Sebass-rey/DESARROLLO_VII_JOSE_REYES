ab379fbc51181d085b973b54dcee7948575552d1
Sebass-rey <jvsrhdh32@gmail.com>
2025-09-12 17:44:53 -0500

    Ejercicio de perfil de usuario completado

---
+++ TALLERES/TALLER_2/perfil_usuario.php
@@ -0,0 +1,28 @@
+<?php
+// Definición de variables
+$nombre = "José Reyes";
+$edad = 24;
+$ciudad = "Panamá";
+
+// Definición de constante
+define("OCUPACION", "Aboagado");
+
+// Creación de mensaje usando diferentes métodos de concatenación e impresión
+$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+$mensaje2 = "Vivo en $ciudad y soy " . OCUPACION . ".";
+
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
+
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $ciudad, PROFESION);
+
+echo "<br>Información de debugging:<br>";
+var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($ciudad);
+echo "<br>";
+var_dump(OCUPACION);
+echo "<br>";
+?>
